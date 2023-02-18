48928638652be6e1a4b16e3c13070d65c9d8dadf
Yacine El Hakim Haddouche <yacinehaddouche12@gmail.com>
2023-02-18 10:13:41 +0100

    added visual improvements for mobile devices

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,5 @@
 
+
 export default function Card() {
     
     return(
@@ -6,18 +7,18 @@ export default function Card() {
             <div className="sm:basis-1/2 overflow-hidden">
                 <picture>
                     <source srcSet="/image-product-desktop.jpg" media="(min-width: 640px)"/>
-                    <img className="object-cover w-full sm:h-full transition ease-in-out duration-1000 group-hover:scale-125" src="/image-product-mobile.jpg" alt="perfume-img" />
+                    <img className="object-cover w-full sm:h-full transition ease-in-out duration-1000 sm:group-hover:scale-125" src="/image-product-mobile.jpg" alt="perfume-img" />
                 </picture>
             </div>
             <div className="flex flex-col sm:basis-1/2 bg-white p-7 ">
-                <p className="text-[#6c7289] uppercase font-montserrat text-xs font-medium tracking-[.3em]">Perfume</p>
-                <h1 className="text-[#1c232b] font-fraunces font-bold text-3xl leading-8 mt-[18px]">Gabrielle Essence Eau De Parfum</h1>
-                <p className="text-[#6c7289] font-montserrat text-[13px] font-medium mt-[18px]">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
-                <div className="flex items-center space-x-4 mt-[18px]">
-                    <h1 className="text-[#3c8067] font-fraunces font-bold text-3xl">$149.99</h1>
+                <p className="text-[#6c7289] uppercase font-montserrat text-xs font-medium sm:tracking-[.3em] tracking-[.5em]">Perfume</p>
+                <h1 className="text-[#1c232b] font-fraunces font-bold text-4xl sm:leading-8 leading-9 mt-[18px]">Gabrielle Essence Eau De Parfum</h1>
+                <p className="text-[#6c7289] font-montserrat sm:text-[13px] text-md font-medium mt-[18px]">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+                <div className="flex items-center sm:space-x-4 space-x-5 mt-[18px]">
+                    <h1 className="text-[#3c8067] font-fraunces font-bold sm:text-3xl text-4xl">$149.99</h1>
                     <p className="text-[#6c7289] font-montserrat font-medium text-[13px] line-through">$169.99</p>
                 </div>
-                <button className="bg-[#3c8067] hover:bg-[#1c3b2a] active:bg-[#1c3b2a] flex items-center justify-center py-3 space-x-3 rounded-lg mt-[18px]">
+                <button className="bg-[#3c8067] sm:hover:bg-[#1c3b2a] active:bg-[#1c3b2a] flex items-center justify-center py-3 space-x-3 rounded-lg mt-[18px]">
                     <svg width="15" height="16" xmlns="http://www.w3.org/2000/svg">
                         <path d="M14.383 10.388a2.397 2.397 0 0 0-1.518-2.222l1.494-5.593a.8.8 0 0 0-.144-.695.8.8 0 0 0-.631-.28H2.637L2.373.591A.8.8 0 0 0 1.598 0H0v1.598h.983l1.982 7.4a.8.8 0 0 0 .799.59h8.222a.8.8 0 0 1 0 1.599H1.598a.8.8 0 1 0 0 1.598h.943a2.397 2.397 0 1 0 4.507 0h1.885a2.397 2.397 0 1 0 4.331-.376 2.397 2.397 0 0 0 1.12-2.021ZM11.26 7.99H4.395L3.068 3.196h9.477L11.26 7.991Zm-6.465 6.392a.8.8 0 1 1 0-1.598.8.8 0 0 1 0 1.598Zm6.393 0a.8.8 0 1 1 0-1.598.8.8 0 0 1 0 1.598Z" fill="#FFF"/>
                     </svg>
